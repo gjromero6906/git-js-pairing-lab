16b04f00eaedaec6ffbd53db3519657d6bb339e2
gjromero6906 <gjr6906@gmail.com>
2025-10-01 14:58:37 -0400

    added muli of three or five

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//1
 //Code your solutions in this file
 function fiveToOneHundred(){
     for(let i= 5;i<101;i++){
@@ -5,12 +6,20 @@ function fiveToOneHundred(){
     }
 }
 fiveToOneHundred();
-
+//2
 // Write a function `multiplesOfThree` that console logs all positive numbers to 100 that is a multiple of 3.
 // > Multiples of 3 are: `3, 6, 9, 12, 15, ...`
 function multiplesOfThree(){
     for(let i=3 ; i <= 100; i+=3){
         console.log(i)
     }}
-    multiplesOfThree(100)
-    
+multiplesOfThree();
+//3
+function multiplesOfThreeOrFive(){
+    for(let i =0;i<=100;i++){
+        if(i%3===0 || i%5===0){
+            console.log(i);
+        }
+    }
+}
+multiplesOfThreeOrFive();
